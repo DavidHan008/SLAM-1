23f4bfe9b140b093bcc4ff099db6c1bbe78a4a68
Ole Egholm Pedersen <olped16@student.sdu.dk>
2020-03-26 11:02:44 +0100

    Test to updload files to git

---
+++ main.py
@@ -0,0 +1,42 @@
+import visual_odometry
+
+# TRACKING
+# ORB Extraction
+# Initial Pose Estimation from Previous Frame
+# OR
+# Initial Pose Estimation via Global Relocalization
+# Track Local Map
+# New KeyFrame Decision
+
+
+# LOCAL MAPPING
+# KeyFrame Insertion
+# Recent MapPoints Culling
+# New MapPoint Creation
+# Local BA
+# Local KeyFrame Culling
+
+
+
+
+# LOOP CLOSURE
+# Loop Candidates Detection
+# Compute Similarity Transform
+# Loop Fusion
+# Essential Graph Optimization
+
+
+
+
+# KeyFrame
+# Includes a pose
+# (Includes camera intrinsics)
+# Includes ORB Features
+
+# COVISIBILITY GRAPH
+
+
+# ESSENTIAL GRAPH
+
+
+# MAPPOINT
