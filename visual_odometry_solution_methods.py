3e9cdd225e0259a0d4db03ada9c6b49a7afea62a
JanusSyrak <44056384+JanusSyrak@users.noreply.github.com>
2020-04-15 13:18:37 +0200

    Changed all methods in vosm.py so they don't require a base class to run.

--- visual_odometry_solution_methods.py
+++ visual_odometry_solution_methods.py
@@ -6,18 +6,6 @@ from matplotlib.path import Path
 import matplotlib.patches as patches
 
 
-def __init__(self, data_dir):
-    self.K, self.P = self._load_calib(os.path.join(data_dir, 'calib.txt'))
-    self.gt_poses = self._load_poses(os.path.join(data_dir, 'poses.txt'))
-    self.images = self._load_images(os.path.join(data_dir, 'images'))
-
-    self.orb = cv2.ORB_create(3000)
-    FLANN_INDEX_LSH = 6
-    index_params = dict(algorithm=FLANN_INDEX_LSH, table_number=6, key_size=12, multi_probe_level=1)
-    search_params = dict(checks=50)
-    self.flann = cv2.FlannBasedMatcher(indexParams=index_params, searchParams=search_params)
-
-
 def load_calib(filepath):
     with open(filepath, 'r') as f:
         params = np.fromstring(f.readline(), dtype=float, sep=' ')
@@ -28,8 +16,8 @@ def load_calib(filepath):
         K_r = P_r[0:3, 0:3]
     return K_l, P_l, K_r, P_r
 
-@staticmethod
-def _load_poses(filepath):
+
+def load_poses(filepath):
     poses = []
     with open(filepath, 'r') as f:
         for line in f.readlines():
@@ -39,36 +27,38 @@ def _load_poses(filepath):
             poses.append(T)
     return poses
 
-@staticmethod
-def _load_images(filepath):
+
+def load_images(filepath):
     image_paths = [os.path.join(filepath, file) for file in sorted(os.listdir(filepath))]
     return [cv2.imread(path, cv2.IMREAD_GRAYSCALE) for path in image_paths]
 
-@staticmethod
-def _form_transf(R, t):
+
+def form_transf(R, t):
     T = np.eye(4, dtype=np.float)
     T[:3, :3] = R
     T[:3, 3] = t
     return T
 
-@staticmethod
+
 def draw_matches(img1, kp1, img2, kp2, matches):
     matches = sorted(matches, key = lambda x:x.distance)
     vis_img = cv2.drawMatches(img1, kp1, img2, kp2, matches[:100], None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
     cv2.imshow('Matches', vis_img)
     cv2.waitKey(0)
 
-def get_pose(self, q1, q2):
-    E, _ = cv2.findEssentialMat(q1, q2, self.K, threshold=1)
-    R, t = self.decomp_essential_mat(E, q1, q2)
-    # inliers, R, t, mask = cv2.recoverPose(E, q1, q2, self.K)
-    return self._form_transf(R, np.squeeze(t))
 
-def decomp_essential_mat(self, E, q1, q2):
+def get_pose(q1, q2, K):
+    E, _ = cv2.findEssentialMat(q1, q2, K, threshold=1)
+    R, t = decomp_essential_mat(E, q1, q2)
+    # inliers, R, t, mask = cv2.recoverPose(E, q1, q2, self.K)
+    return form_transf(R, np.squeeze(t))
+
+
+def decomp_essential_mat(E, q1, q2, K, P):
     def sum_z(R, t):
-        T = self._form_transf(R, t)
-        P = np.matmul(np.concatenate((self.K, np.zeros((3, 1))), axis=1), T)
-        hom_Q1 = cv2.triangulatePoints(self.P, P, q1.T, q2.T)
+        T = form_transf(R, t)
+        P2 = np.matmul(np.concatenate((K, np.zeros((3, 1))), axis=1), T)
+        hom_Q1 = cv2.triangulatePoints(P, P2, q1.T, q2.T)
         hom_Q2 = np.matmul(T, hom_Q1)
         return sum(hom_Q1[2, :] / hom_Q1[3, :] > 0) + sum(hom_Q2[2, :] / hom_Q2[3, :] > 0)
 
